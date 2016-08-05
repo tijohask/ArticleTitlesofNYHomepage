f54988a8f42e23a2f3150038aa3c57883a973b4a
Johnny Carlo <timothyjhask@gmail.com>
2016-08-04 21:46:11 -0400

    Prints Headlines
    Program selects all elements with the class "story-heading" and prints out any text they have.

--- ArticleTitlesofNYHomepage.py
+++ ArticleTitlesofNYHomepage.py
@@ -2,7 +2,14 @@
 #print a list of all the article headlines.
 
 from bs4 import BeautifulSoup
+from bs4 import NavigableString
 from urllib.request import urlopen
 
 doc = urlopen("http://nytimes.com").read()
-print (doc)
+#print(doc)
+soup = BeautifulSoup(doc, "html.parser")
+print(soup.title.text)
+title_list = soup.select(".story-heading")
+for item in title_list:
+	if(item.string != 'None'):
+		print(item.text.strip())
