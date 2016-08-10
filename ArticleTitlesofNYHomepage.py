cf05a790737f06abed91e94f2d1ccbf82494de53
Johnny Carlo <timothyjhask@gmail.com>
2016-08-09 23:26:29 -0400

    Prints headlines to text document

--- ArticleTitlesofNYHomepage.py
+++ ArticleTitlesofNYHomepage.py
@@ -8,8 +8,12 @@ from urllib.request import urlopen
 doc = urlopen("http://nytimes.com").read()
 #print(doc)
 soup = BeautifulSoup(doc, "html.parser")
+open_file = open('headlines.txt', 'w')
 print(soup.title.text)
+open_file.write(soup.title.text + "\n\n")
 title_list = soup.select(".story-heading")
 for item in title_list:
 	if(item.string != 'None'):
-		print(item.text.strip())
+		open_file.write(item.text.strip() + "\n\n")
+open_file.close()
+print("Done!")
