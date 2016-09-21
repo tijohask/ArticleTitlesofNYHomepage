cd349a5d2346682291c6b9751af51dd4f8490b60
Johnny Carlo <timothyjhask@gmail.com>
2016-09-20 22:35:30 -0400

    Added comments, rearranged code

--- ArticleTitlesofNYHomepage.py
+++ ArticleTitlesofNYHomepage.py
@@ -5,15 +5,30 @@ from bs4 import BeautifulSoup
 from bs4 import NavigableString
 from urllib.request import urlopen
 
+#Open online html file
 doc = urlopen("http://nytimes.com").read()
-#print(doc)
+
+#Parse it into bS4
 soup = BeautifulSoup(doc, "html.parser")
-open_file = open('headlines.txt', 'w')
+
+#Print webpage title to user
 print(soup.title.text)
-open_file.write(soup.title.text + "\n\n")
+
+#Create a list with all the headline objects in the soup
 title_list = soup.select(".story-heading")
+
+#Open text file for output
+open_file = open('headlines.txt', 'w')
+
+#Print webpage title to text file
+open_file.write(soup.title.text + "\n\n")
+
+#Go through the list, printing the headlines to the output text file
 for item in title_list:
+	#Make sure that the item has a headline
 	if(item.string != 'None'):
 		open_file.write(item.text.strip() + "\n\n")
+
+#Close output text file
 open_file.close()
 print("Done!")
